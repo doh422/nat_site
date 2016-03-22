28a40e0163590bbd02b5ec5f4f773ecca9d6cfd0
Daniel Oh <doh422@gmail.com>
2016-03-21 18:00:15 -0700

    added scrolling animation for navbar links

--- script.js
+++ script.js
@@ -15,11 +15,34 @@ var main = function() {
 
 	// stick nav
 	$(window).scroll(function() {
-		if ($(this).scrollTop() >= 90) {
+		if ($(this).scrollTop() >= 105) {
 			$('#main-nav').addClass('sticky');
 		} else {
 			$('#main-nav').removeClass('sticky');
 		}
+	});
+
+	// scrolling animation function
+	function scrollToDiv(element, navheight) {
+		var offset = element.offset();
+		var offsetTop = offset.top;
+		var totalScroll = offsetTop - navheight;
+		$('body, html').animate({
+			scrollTop: totalScroll
+		}, 1000);
+	}
+
+	$('#main-nav .nav li a').click(function() {
+		// store href value in el variable
+		var el = $(this).attr('href');
+		// wrap variable so we can use jQuery
+		var elWrapped = $(el);
+
+		// use scroll function
+		scrollToDiv(elWrapped,70);
+
+		// return false so link doesn't go directly to page without animation
+		return false;
 	});
 
 };
